66ced89c384c22124a89c306f10b686a561fef23
René Majewski <rene.majewski@gmx.de>
2018-07-03 23:13:00 +0200

    Create MinecraftLog project structure

---
+++ src/main/java/de/rene_majewski/minecraft_log/App.java
@@ -0,0 +1,13 @@
+package de.rene_majewski.minecraft_log;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
